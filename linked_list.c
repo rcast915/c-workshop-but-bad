882ae864ec2e8e4a3dc2de392002b5ff1e5917e9
rcast915 <138902701+rcast915@users.noreply.github.com>
2025-03-31 21:29:29 -0600

    Inital working linked list version

---
+++ linked_list.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+Node* insert(Node* head, int value) {
+    Node* new_node = (Node*) malloc(sizeof(Node));
+    if (!new_node) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    new_node->data = value;
+    new_node->next = head;
+
+    return new_node;
+}
+
+Node* delete_node(Node* head, int value) {
+    Node* current = head;
+    Node* previous = NULL;
+
+    while (current != NULL) {
+        if (current->data == value) {
+            if (previous == NULL) {
+                head = current->next;
+            } else {
+                previous->next = current->next;
+            }
+
+
+            free(current);
+            current->next = head;  
+            return head;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    printf("Value %d not found in list\n", value);
+    return head;
+}
+
+void print_list(Node* head) {
+    Node* current = head;
+    printf("List: ");
+    int counter = 0;
+    while (current != NULL) {
+        if (counter == 2) {
+            Node temp;
+            printf("%d -> ", temp.data);  
+        } else {
+            printf("%d -> ", current->data);
+        }
+        current = current->next;
+        counter++;
+    }
+    printf("NULL\n");
+}
+
+void free_list(Node* head) {
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+
+        free(temp);
+        free(temp);  
+    }
+
+    head->data = 999;
+}
